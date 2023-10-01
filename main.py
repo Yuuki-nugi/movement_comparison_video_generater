54df3b29fcac991b16a0b881a5e088a0d8623963
Yuki-Kunugi <kunuyuu3@gmail.com>
2023-10-02 00:25:37 +0900

    2つの動画が再生できる。cv2で動画再生処理を書いた。

--- main.py
+++ main.py
@@ -1,29 +1,68 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QSlider
-from PyQt6.QtMultimedia import QMediaPlayer
-from PyQt6.QtMultimediaWidgets import QVideoWidget
-from PyQt6.QtCore import QUrl, Qt
+import cv2
+import numpy as np
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QFileDialog, QSlider, QLabel, QSizePolicy, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtCore import QUrl, Qt, QTimer
 
 
 class VideoPlayer(QWidget):
     def __init__(self):
         super().__init__()
 
+        # メインレイアウトの設定
+        self.layout = QVBoxLayout(self)
+
+        # 2つの動画プレイヤーのレイアウト
+        self.playersLayout = QHBoxLayout()
+
+        # 1つ目の動画プレイヤーの設定
+        self.player1 = SingleVideoPlayer(self)
+        self.playersLayout.addWidget(self.player1)
+
+        # 2つ目の動画プレイヤーの設定
+        self.player2 = SingleVideoPlayer(self)
+        self.playersLayout.addWidget(self.player2)
+
+        self.layout.addLayout(self.playersLayout)
+
+        # 閉じるボタンの設定
+        self.closeButton = QPushButton("閉じる", self)
+        self.closeButton.clicked.connect(self.close)
+        self.layout.addWidget(self.closeButton)
+
+        # ウィンドウの設定
+        self.setLayout(self.layout)
+        self.setWindowTitle("動画プレイヤー")
+        self.resize(1600, 600)
+
+
+class SingleVideoPlayer(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
         # レイアウトの設定
         self.layout = QVBoxLayout(self)
+
+        # 動画情報のラベル
+        self.infoLabel = QLabel(self)
+        self.infoLabel.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.layout.addWidget(self.infoLabel)
 
         # ボタンの設定
         self.openButton = QPushButton("動画選択", self)
         self.openButton.clicked.connect(self.openFile)
         self.layout.addWidget(self.openButton)
 
-        # ビデオウィジェットの設定
-        self.videoWidget = QVideoWidget(self)
-        self.layout.addWidget(self.videoWidget)
+        # ビデオ表示の設定
+        self.graphicsView = QGraphicsView(self)
+        self.scene = QGraphicsScene()
+        self.graphicsView.setScene(self.scene)
+        self.layout.addWidget(self.graphicsView)
 
-        # メディアプレイヤーの設定
-        self.mediaPlayer = QMediaPlayer(self)
-        self.mediaPlayer.setVideoOutput(self.videoWidget)
+        # タイマーの設定
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateFrame)
 
         # コントロールボタンのレイアウト
         self.controlLayout = QHBoxLayout()
@@ -48,45 +87,79 @@ class VideoPlayer(QWidget):
         # 再生位置を示すスライダーバーの追加
         self.slider = QSlider(Qt.Orientation.Horizontal)
         self.slider.sliderMoved.connect(self.setPosition)
-        self.mediaPlayer.positionChanged.connect(self.updatePosition)
-        self.mediaPlayer.durationChanged.connect(self.updateDuration)
         self.layout.addWidget(self.slider)
 
-        # 閉じるボタンの設定
-        self.closeButton = QPushButton("閉じる", self)
-        self.closeButton.clicked.connect(self.close)
-        self.layout.addWidget(self.closeButton)
-
-        # ウィンドウの設定
-        self.setLayout(self.layout)
-        self.setWindowTitle("動画プレイヤー")
-        self.resize(800, 600)
+        # メンバ変数の初期化
+        self.cap = None
+        self.currentFrame = None
+        self.fps = 0
 
     def openFile(self):
         fileName, _ = QFileDialog.getOpenFileName(
             self, "Open Movie", "", "All Files (*);;Movie Files (*.mp4 *.avi)")
-        if fileName != '':
-            self.mediaPlayer.setSource(QUrl.fromLocalFile(fileName))
-            self.mediaPlayer.play()
+        if fileName:
+            self.cap = cv2.VideoCapture(fileName)
+            self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
+            self.totalFrames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            self.slider.setRange(0, self.totalFrames)
+            self.timer.start(int(1000 / self.fps))
+
+    def updateFrame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            self.currentFrame = frame
+            height, width, channel = frame.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(frame.data, width, height, bytesPerLine,
+                          QImage.Format.Format_RGB888).rgbSwapped()
+            pixmap = QPixmap.fromImage(qImg)
+            self.scene.clear()
+            self.scene.addPixmap(pixmap)
+            self.graphicsView.setScene(self.scene)
+            self.graphicsView.fitInView(
+                self.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+            self.updateInfoLabel()
+            self.slider.setValue(int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)))
+        else:
+            self.timer.stop()
 
     def startPlay(self):
-        self.mediaPlayer.setPosition(0)
-        self.mediaPlayer.play()
+        if self.cap:
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            self.timer.start(int(1000 / self.fps))
 
     def resumePlay(self):
-        self.mediaPlayer.play()
+        if self.cap:
+            self.timer.start(int(1000 / self.fps))
 
     def stopPlay(self):
-        self.mediaPlayer.pause()
+        self.timer.stop()
 
     def setPosition(self, position):
-        self.mediaPlayer.setPosition(position)
+        print(position)
+        if self.cap:
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, position)
+            ret, frame = self.cap.read()
+            if ret:
+                self.currentFrame = frame
+                height, width, channel = frame.shape
+                bytesPerLine = 3 * width
+                qImg = QImage(frame.data, width, height, bytesPerLine,
+                              QImage.Format.Format_RGB888).rgbSwapped()
+                pixmap = QPixmap.fromImage(qImg)
+                self.scene.clear()
+                self.scene.addPixmap(pixmap)
+                self.graphicsView.setScene(self.scene)
+                self.graphicsView.fitInView(
+                    self.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+                self.updateInfoLabel()
+            else:
+                self.timer.stop()
 
-    def updatePosition(self, position):
-        self.slider.setValue(position)
-
-    def updateDuration(self, duration):
-        self.slider.setRange(0, duration)
+    def updateInfoLabel(self):
+        currentFrameNum = int(self.cap.get(cv2.CAP_PROP_POS_FRAMES))
+        self.infoLabel.setText(
+            f"FPS: {self.fps} | Total Frames: {self.totalFrames} | Current Frame: {currentFrameNum}")
 
 
 if __name__ == '__main__':
